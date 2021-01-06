31f8bf475a8f5aaf6d3e6f85057e6b6f93d2d704
eazyt <ramohlalen@gmail.com>
2021-01-07 01:04:41 +0200

    initial Commit

---
+++ routes/users.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  // write future users code
+  res.send('respond with a resource');
+});
+
+module.exports = router;
