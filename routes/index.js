2e89ce919f6859bb7aa6c5380d5a509079c626f5
eazyt <ramohlalen@gmail.com>
2021-01-07 02:59:54 +0200

    added productfind route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Product = require('../models/product')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   // insert future code
-  res.render('main/index', { title: 'Express2' });
+  Product.find((err, products) => { 
+    let productsChunks = [];
+    let chunkSize = 3;
+    for (let i = 0; i < products.length; i += chunkSize) { 
+      productsChunks.push(products.slice(i, i + chunkSize));
+    }
+    res.render('main/index', {
+      title: 'Shopping Cart',
+      products: productsChunks
+    });
+  })
 });
 
 module.exports = router;
