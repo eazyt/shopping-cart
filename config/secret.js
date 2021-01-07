5e02198523fc98c9e06597432cdd25d3b6202d4f
eazyt <ramohlalen@gmail.com>
2021-01-07 02:45:43 +0200

    seeding data to database with seeder

--- config/secret.js
+++ config/secret.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
+
 module.exports = {
 
-  database: 'mongodb://172.15.22.82:27017/tamazoun-e',
-  port: 3000,
-  secretKey: "Arash@$@!#@"
+  database: process.env.DB_URI,
+  port: process.env.PORT,
+  secretKey: process.env.SECRET
 }
---
+++ seed/productSeeder.js
@@ -0,0 +1,72 @@
+const Product = require('../models/product');
+const secret = require('../config/secret')
+
+const mongoose = require('mongoose');
+
+mongoose.connect(secret.database, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: true
+  })
+  .then(() => {
+    console.log(`succefully connected to the Database`)
+  })
+  .catch((e) => {
+    console.log(`could not connect to database`, e)
+  })
+
+const products = [
+  new Product({
+  imagePath: 'http://placeimg.com/640/480/science',
+  title: 'The God\'s must be Crazy',
+  description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores omnis consequuntur laborum, qui beatae nostrum cumque optio illo facilis rem quas voluptates quia, necessitatibus aut sapiente rerum. Animi, reprehenderit deleniti?',
+  price: 10.99
+  }),
+  new Product({
+  imagePath: 'http://placeimg.com/640/480/science',
+  title: 'The Avengers - Age of Ultron',
+  description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores omnis consequuntur laborum, qui beatae nostrum cumque optio illo facilis rem quas voluptates quia, necessitatibus aut sapiente rerum. Animi, reprehenderit deleniti?',
+  price: 11.99
+  }),
+  new Product({
+  imagePath: 'http://placeimg.com/640/480/science',
+  title: 'Tron',
+  description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores omnis consequuntur laborum, qui beatae nostrum cumque optio illo facilis rem quas voluptates quia, necessitatibus aut sapiente rerum. Animi, reprehenderit deleniti?',
+  price: 100.99
+  }),
+  new Product({
+  imagePath: 'http://placeimg.com/640/480/science',
+  title: 'The End Game',
+  description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores omnis consequuntur laborum, qui beatae nostrum cumque optio illo facilis rem quas voluptates quia, necessitatibus aut sapiente rerum. Animi, reprehenderit deleniti?',
+  price: 99.10
+  }),
+  new Product({
+  imagePath: 'http://placeimg.com/640/480/science',
+  title: 'The Fast and Slow',
+  description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores omnis consequuntur laborum, qui beatae nostrum cumque optio illo facilis rem quas voluptates quia, necessitatibus aut sapiente rerum. Animi, reprehenderit deleniti?',
+  price: 22.75
+  }),
+  new Product({
+  imagePath: 'http://placeimg.com/640/480/science',
+  title: 'Basic Insticnt',
+  description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores omnis consequuntur laborum, qui beatae nostrum cumque optio illo facilis rem quas voluptates quia, necessitatibus aut sapiente rerum. Animi, reprehenderit deleniti?',
+  price: 29.50
+  }),
+]
+
+
+let done = 0;
+
+for (let i = 0; i < products.length; i++) { 
+  products[i].save(() => { 
+    done++
+    if (done === products.length) { 
+      exit();
+    }
+  })
+}
+
+function exit() { 
+  mongoose.disconnect();
+}
