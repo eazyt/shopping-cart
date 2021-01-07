b24404272392357a28b96eab3d0f7cfa93c8e4f9
eazyt <ramohlalen@gmail.com>
2021-01-07 13:47:10 +0200

    added product views to home route

--- routes/index.js
+++ routes/index.js
@@ -9,13 +9,52 @@ router.get('/', function (req, res, next) {
     let productsChunks = [];
     let chunkSize = 3;
     for (let i = 0; i < products.length; i += chunkSize) { 
+      // console.log(products + "THIS IS TITLES")
       productsChunks.push(products.slice(i, i + chunkSize));
     }
+    console.log(productsChunks[1] + 'THIS PRODUCTSCHUNKS')
+    // console.log(productsChunks + 'THIS IS CHUNKS')
     res.render('main/index', {
       title: 'Shopping Cart',
       products: productsChunks
     });
   })
+
+
+
+
+
+
+
+
+  // This works with
+  //   <% products.forEach(function (product) { %>
+  //        <%= product.imagePath %>
+  //        <%= product.price %>
+  //   <% } %>
+  // Product.find((err, products) => { 
+  //   res.render('main/index', {
+  //     title: 'Shopping Cart',
+  //     products: products
+  //   });
+  // })
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
 
 module.exports = router;
